638659b000d3806cffbfd43c77e1b562cb1ae308
Torfand <torfin@getacademy.no>
2020-01-09 10:58:39 +0100

    update

--- Js/model.js
+++ Js/model.js
@@ -1,4 +1,20 @@
 //model
-let personaList = {
-    personaInfo : {
-    assignedID : 1 , personaName : 'Arsene', personaArcana : 'Fool'} }
+var model = {
+    personaInfo: {
+        personaList: [
+            {
+                Name: 'Arsene', Arcana: 'Fool',
+                attributes: { str: 2, mag: 2, end: 2, agi: 3, lck: 1}
+            },
+            {
+                Name: 'Jack-o-Lantern', Arcana: 'Magician',
+                attributes: { str: 2, mag: 3, end: 3, agi: 3, lck: 3}
+            }
+
+        ]
+        // attributes: { str: 2, mag: 2, end: 3, lck: 1}, 
+
+
+    }
+
+}
--- Js/persona.js
+++ Js/persona.js
@@ -11,32 +11,47 @@ function displayTable(){
         Arcana
       </div>
       <div class="cell">
-        Occupation
+        Strenght
       </div>
       <div class="cell">
-        Location
+        Magic
+      </div>
+      <div class="cell">
+        Endurance
+      </div>
+      <div class="cell">
+        Agility
+      </div>
+      <div class="cell">
+        Luck
+      </div>
+      
+    </div>
     `
     
     document.getElementById('wrapper').innerHTML = html;
 }
  function diplayPersonaContent(){
+   for (personas of model.personaInfo.personaList){
       html += `
-    </div>
-    </div>
     <div class="row">
-      <div class="cell" data-title="Name">
-        ${personaList.personaInfo.personaName}
+      <div class="cell" data-title="Name">${personas.Name} 
       </div>
-      <div class="cell" data-title="Age">
-      ${personaList.personaInfo.personaArcana}
+      <div class="cell" data-title="Arcana">${personas.Arcana}
       </div>
-      <div class="cell" data-title="Occupation">
-        Freelance Web Developer
+      <div class="cell" data-title="Strenght">${personas.attributes.str}
       </div>
-      <div class="cell" data-title="Location">
-        Brookline, MA
+      <div class="cell" data-title="Magic">${personas.attributes.mag}
       </div>
-    </div>`
+      <div class="cell" data-title="Endurance">${personas.attributes.end}
+      </div>
+      <div class="cell" data-title="Agility">${personas.attributes.agi}
+      </div>
+      <div class="cell" data-title="Luck">${personas.attributes.lck}
+        
+      </div>
+    </div>` ;}
+    console.log(html)
     document.getElementById('wrapper').innerHTML = html;
 
  }
