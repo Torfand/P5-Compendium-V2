a1c149ccc69af3408fe4b7ddc121ecab35485a47
Torfand <torfin@getacademy.no>
2020-01-09 11:03:08 +0100

    Update model.js

--- Js/model.js
+++ Js/model.js
@@ -9,7 +9,19 @@ var model = {
             {
                 Name: 'Jack-o-Lantern', Arcana: 'Magician',
                 attributes: { str: 2, mag: 3, end: 3, agi: 3, lck: 3}
-            }
+            },
+            {
+                Name: 'Pixie', Arcana: 'Lovers',
+                attributes: { str: 1, mag: 3, end: 3, agi: 4, lck: 2}
+            },
+            {
+                Name: 'Agathion', Arcana: 'Chariot',
+                attributes: { str: 3, mag: 4, end: 5, agi: 6, lck: 3}
+            },
+            {
+                Name: 'Mandrake', Arcana: 'Death',
+                attributes: { str: 2, mag: 3, end: 3, agi: 4, lck: 4}
+            },
 
         ]
         // attributes: { str: 2, mag: 2, end: 3, lck: 1}, 
