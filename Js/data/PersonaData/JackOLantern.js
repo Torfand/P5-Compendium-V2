84774ab85c28e05347a33d9fdf6dd790cbca760b
Torfand <57662236+Torfand@users.noreply.github.com>
2020-01-15 19:03:25 +0100

    Update to JackOLantern.js Extracted functions from inner/local to Global to reduce function Clutter
    Update to JackOLantern.js Extracted functions from inner/local to Global to reduce function Clutter

--- Js/data/PersonaData/JackOLantern.js
+++ Js/data/PersonaData/JackOLantern.js
@@ -26,45 +26,60 @@ function JackOLantern() {
                 <td>${luck}</td>
                         </tr> 
                     </table>`;
-    elementsTable();
+    //elementsTable();
+
+
+
+    elementsTable(physical, gun, fire, ice, electric, wind, psychic, nuclear, bless, curse);
+    //skillTable();
+
+
+
+    skillTable();
+    // html+= `</table>`       
+    //fuse();
+
+
+
+    fuse();
+
     document.getElementById('wrapper').innerHTML = html;
-
-
-    function elementsTable() {
-        html += `
+    console.log(html)
+}
+function elementsTable(physical, gun, fire, ice, electric, wind, psychic, nuclear, bless, curse) {
+    html += `
         <h1 class="profileElementsHeader">Elements :</h1>
         <table class="profileElements">
         <tr>
-            <th>Physical</th>
-            <th>Gun</th>
-            <th>Fire</th>
-            <th>Ice</th>
-            <th>Electric</th>
-            <th>Wind</th>
-            <th>Psychic</th>
-            <th>Nuclear</th>
-            <th>Bless</th>
-            <th>Curse</th>
+        <th>Physical</th>
+        <th>Gun</th>
+        <th>Fire</th>
+        <th>Ice</th>
+        <th>Electric</th>
+        <th>Wind</th>
+        <th>Psychic</th>
+        <th>Nuclear</th>
+        <th>Bless</th>
+        <th>Curse</th>
         </tr>
         <tr>
-            <td>${physical}</td>
-            <td>${gun}</td>
-            <td>${fire}</td>
-            <td>${ice}</td>
-            <td>${electric}</td>
-            <td>${wind}</td>
-            <td>${psychic}</td>
-            <td>${nuclear}</td>
-            <td>${bless}</td>
-            <td>${curse}</td>
-        </tr> 
-        </table>`
-        skillTable();
-    }
+        <td>${physical}</td>
+        <td>${gun}</td>
+        <td>${fire}</td>
+        <td>${ice}</td>
+        <td>${electric}</td>
+        <td>${wind}</td>
+        <td>${psychic}</td>
+        <td>${nuclear}</td>
+        <td>${bless}</td>
+        <td>${curse}</td>
+            </tr> 
+            </table>`;
+}
 
-    function skillTable() {
-        html +=
-            ` <h1 class="profileSkillHeader">Skills :</h1>
+function skillTable() {
+    html +=
+        ` <h1 class="profileSkillHeader">Skills :</h1>
         <table class="profileSkills">
         <tr>
         <th>Level</th>
@@ -73,65 +88,59 @@ function JackOLantern() {
         <th>Description</th>
         <th>Cost</th>  
         </tr> `;
-        for (var skill in skillData) {
-            for (var thing in skillData[skill].persona) {
-
-                if (skillData[skill].persona.hasOwnProperty('Jack_O_Lantern') && thing == 'Jack_O_Lantern') {
-                    html += `
-        <tr>
-            <td>${skillData[skill].persona.Jack_O_Lantern}</td>
-            <td>${skillData[skill].name}</td>
-            <td>${skillData[skill].type}</td>
-            <td>${skillData[skill].description}</td>
-            <td>${skillData[skill].cost}</td>
-        </tr>`;
-                }
+    for (var skill in skillData) {
+        for (var thing in skillData[skill].persona) {
+            if (skillData[skill].persona.hasOwnProperty('Jack_O_Lantern') && thing == 'Jack_O_Lantern') {
+                html += `
+                    <tr>
+                    <td>${skillData[skill].persona.Jack_O_Lantern}</td>
+                    <td>${skillData[skill].name}</td>
+                    <td>${skillData[skill].type}</td>
+                    <td>${skillData[skill].description}</td>
+                    <td>${skillData[skill].cost}</td>
+                    </tr>`;
             }
-
-
         }
-        // html+= `</table>`       
-        fuse();
     }
+}
 
-    function fuse() {
-        html += `
+
+function fuse() {
+    html += `
         <h1 class="profileFuseFromHeader">Fuse From This:</h1>
         <table class="profileFuseFrom">
         <tr>
-            <th>Cost</th>
-            <th>Ingredients</th>
-        </tr>`
-        for (fusecombos of fuseData.fuseTo.Jack_O_Lantern) {
-            html +=
-                `<tr>
+        <th>Cost</th>
+        <th>Ingredients</th>
+        </tr>`;
+    for (fusecombos of fuseData.fuseTo.Jack_O_Lantern) {
+        html +=
+            `<tr>
             <td>${fusecombos.fuseCost}</td>
             <td>${fusecombos.combo}</td>
-        </tr>
-        `;
-        };
-        html +=
-            `</table>
+            </tr>
+            `;
+    }
+    ;
+    html +=
+        `</table>
         <h1 class="profileFuseToHeader">Fuse To This :</h1>
         <table class="profileFuseTo">
         <tr>
-            <th>Cost</th>
-            <th>Ingredients</th>
+        <th>Cost</th>
+        <th>Ingredients</th>
         </tr>
-        `
-        for (fusecombos of fuseData.fuseFrom.Jack_O_Lantern) {
-            html += `
-        <tr>
+        `;
+    for (fusecombos of fuseData.fuseFrom.Jack_O_Lantern) {
+        html += `
+            <tr>
             <td>${fusecombos.fuseCost}</td>
             <td>${fusecombos.combo}</td>
-        </tr>`;
-        };
-
-
-        html += `
-      </table>
-        </div>`;
+            </tr>`;
     }
-    console.log(html)
+    ;
+    html += `
+        </table>
+        </div>`;
 }
 
